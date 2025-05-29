50b35f74dc9b1b91bb42bf3380e52cb44693eddf
Nick Dunn <nickdunn2@gmail.com>
2025-05-29 09:07:19 -0500

    feat(app): connect fe to be

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+
+const API_URL = 'http://localhost:8000'
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(`${API_URL}/posts/`)
+      .then(response => {
+        const json = response.json()
+        console.log(json)
+        if (response.ok) {
+          return json
+        }
+        throw response
+      })
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error fetching posts:', error))
+  }, [])
+
   return (
     <div className="app">
       <h1>Hello World</h1>
