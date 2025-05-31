9a23f0092f75079795490aa46de4d6434dfb1cb9
Nick Dunn <nickdunn2@gmail.com>
2025-05-31 18:20:42 -0500

    feat(post): add caption and comments

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,6 +4,7 @@ import { API_URL } from '../../constants'
 
 const Post = ({ post }) => {
   const [imageUrl, setImageUrl] = useState('')
+  const [comments, setComments] = useState([])
 
   useEffect(() => {
     if (post.image_url_type === 'absolute') {
@@ -13,9 +14,23 @@ const Post = ({ post }) => {
     }
   }, [post.image_url, post.image_url_type])
 
+  useEffect(() => {
+    setComments(post.comments)
+  }, [post.comments])
+
   return (
     <div className="post">
       <img className="post-image" src={imageUrl} alt={post.title} />
+      <p className="post-caption">{post.caption}</p>
+      <div className="post-comments">
+        {comments.map(comment => (
+          <div className="post-comment" key={comment.id}>
+            <p>
+              <strong>{comment.username}:</strong> {comment.text}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
